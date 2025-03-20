ca9471f3c143940e9ea14caae524ba4804ba8eda
大河 <yin-bp@163.com>
2025-03-21 00:17:37 +0800

    修改通过context设置的index和indexType不起作用问题

---
+++ src/main/java/org/frameworkset/elasticsearch/imp/minio/MinioTest.java
@@ -0,0 +1,54 @@
+package org.frameworkset.elasticsearch.imp.minio;
+/**
+ * Copyright 2025 bboss
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.frameworkset.nosql.minio.Minio;
+import org.frameworkset.nosql.minio.MinioConfig;
+import org.frameworkset.nosql.minio.MinioHelper;
+
+/**
+ * <p>Description: </p>
+ * <p></p>
+ *
+ * @author biaoping.yin
+ * @Date 2025/3/20
+ */
+public class MinioTest {
+    public static void main(String[] args) throws Exception {
+        //1. 初始化Minio数据源chan_fqa，用来操作向量数据库，一个Minio数据源只需要定义一次即可，后续通过名称chan_fqa反复引用，多线程安全
+        // 可以通过以下方法定义多个Minio数据源，只要name不同即可，通过名称引用对应的数据源
+        MinioConfig minioConfig = new MinioConfig();
+
+        minioConfig.setEndpoint("http://172.24.176.18:9000");
+
+        minioConfig.setName("miniotest");
+        minioConfig.setAccessKeyId("N3XNZFqSZfpthypuoOzL");
+        minioConfig.setSecretAccesskey("2hkDSEll1Z7oYVfhr0uLEam7r0M4UWT8akEBqO97");
+        minioConfig.setConnectTimeout(5000);
+        minioConfig.setReadTimeout(5000);
+        minioConfig.setWriteTimeout(5000);
+
+        minioConfig.setMaxFilePartSize(10*1024*1024*1024);
+        boolean result = MinioHelper.init(minioConfig);//
+
+
+        Minio minio = MinioHelper.getMinio("miniotest");
+        minio.downloadObject("etlfiles","HN_BOSS_TRADE_202501092032_000001.txt","C:/data/filedown/HN_BOSS_TRADE_202501092032_000005.txt");
+        minio.createBucket("filedown");
+        minio.uploadObject("C:/data/filedown/HN_BOSS_TRADE_202501092032_000004.txt","filedown","HN_BOSS_TRADE_202501092032_000001.txt");
+        
+    }
+}
